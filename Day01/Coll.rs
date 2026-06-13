cfd8b6cec6ad1fa553c2d07e0772d5ef2975d040
fabio <fabio@PiratePort>
2026-06-13 17:04:46 -0300

    corret warning Collz

--- Day01/Coll.rs
+++ Day01/Coll.rs
@@ -1,10 +1,10 @@
-fn collatz(mut n: i32) -> i32 {
+fn collatz(n: i32) -> i32 {
     if 1 == n {
-        return 1;
+        1
     } else if 0 == (n % 2) {
-        return collatz(n/2) + 1;
+        collatz(n/2) + 1
     } else {
-        return collatz(3*n+1) + 1 ;
+        collatz(3*n+1) + 1
     }
 }
 
