1d39f96e2dffc29ec323efc0b754758eff869e35
Fabio Lima <fabiolima39@gmail.com>
2026-07-15 23:32:13 -0300

    clousure traits

---
+++ Day02/ClosureSyntax/ClosureTraits.rs
@@ -0,0 +1,12 @@
+
+fn apply_and_log(
+    func: impl FnOnce(&'static str) -> String,
+    func_name: &'static str,
+    input: &'static str,
+) {
+    println!("Calling {func_name}({input}): {}", func(input))
+}
+
+fn main() {
+
+}
