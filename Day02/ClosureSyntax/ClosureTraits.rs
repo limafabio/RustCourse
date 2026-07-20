b9892de9082def86476dde916c6693e69cf04404
Fabio Lima <fabiolima39@gmail.com>
2026-07-19 21:10:11 -0300

    Exercises Closure Traits

--- Day02/ClosureSyntax/ClosureTraits.rs
+++ Day02/ClosureSyntax/ClosureTraits.rs
@@ -8,5 +8,25 @@ fn apply_and_log(
 }
 
 fn main() {
+    let suffix = "-itis";
+    let add_suffix = |x| format!("{x}{suffix}");
+    apply_and_log(&add_suffix, "add_suffix", "senior");
+    apply_and_log(&add_suffix, "add_suffix", "appendix");
 
+    let mut v = Vec::new();
+    let mut accumulate = |x| {
+        v.push(x);
+        v.join("/")
+    };
+
+    apply_and_log(&mut accumulate, "accumulate", "red");
+    apply_and_log(&mut accumulate, "accumulate", "green");
+    apply_and_log(&mut accumulate, "accumulate", "blue");
+
+    let take_and_reverse = |prefix| {
+        let mut acc = String::from(prefix);
+        acc.push_str(&v.into_iter().rev().collect::<Vec<_>>().join("/"));
+        acc
+    };
+    apply_and_log(take_and_reverse, "take_and_reverse", "reversed: ");
 }
