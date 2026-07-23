f917e47aaa3a3abf096c113c76c852428fa3acf2
Fabio Lima <fabiolima39@gmail.com>
2026-07-22 22:45:56 -0300

    exercise about hash map

---
+++ Day02/StandardLibraryTypes/HashMap.rs
@@ -0,0 +1,28 @@
+use std::collections::HashMap;
+
+fn main() {
+		let mut page_counts = HashMap::new();
+		page_counts.insert("Adventures of Huckleberry Finn",207);
+		page_counts.insert("Grimms' Fairy Tales", 751);
+		page_counts.insert("Pride and Prejudice", 303);
+
+		if !page_counts.contains_key("Les Misérables") {
+				println!(
+						"We know about {} books, but not Les Misérables.",page_counts.len()
+				);
+		}
+
+		for book in ["Pride and Prejudice", "Alice's Adventure in Wonderland"] {
+				match page_counts.get(book) {
+						Some(count) => println!("{book}: {count} pages"), 
+						None => println!("{book} is unknown."),
+				}
+		}
+
+		for book in ["Pride and Prejudice", "Alice's Adventure in Wonderland"] {
+				let page_count: &mut i32 = page_counts.entry(book).or_insert(0);
+				*page_count += 1;
+		}
+
+		dbg!(page_counts);
+}
