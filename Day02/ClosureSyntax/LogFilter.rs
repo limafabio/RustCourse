d209e3f5f6f999b909794a7e94569519b034e442
Fabio Lima <fabiolima39@gmail.com>
2026-07-22 22:51:23 -0300

    organize folder StandardLibrary

---
+++ Day02/ClosureSyntax/LogFilter.rs
@@ -0,0 +1,24 @@
+
+pub trait Logger {
+    fn log(&self, verbosity: u8, message: &str);
+}
+
+struct StderrLogger;
+
+impl Logger for StderrLogger {
+    fn log(&self, verbosity: u8, message: &str) {
+        eprintln!("verbosity={verbosity}: {message}");
+    }
+}
+
+impl Filter (){
+
+}
+
+
+fn main() {
+    let logger = Filter::new(StderrLogger, |_verbosity, ms| msg.contains("yikes"));
+    logger.log(5, "FYI");
+    logger.log(1, "yikes, something went wrong");
+    logger.log(2, "uhoh");
+}
